b0e96416dea23d41a9111d210dacde4f430b1678
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-30 18:16:50 +0300

    FileReader class is  rdy

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,20 +1,24 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileReader {
-    public static List<String> readFile(String filePath){
+    public static List<String> readFile(String filePath) {
         List<String> stringList = new ArrayList<>();
-
+        File file = new File(filePath);
         try {
-            BufferedReader reader = new BufferedReader(new java.io.FileReader(filePath));
-            for (String line; (line = reader.readLine()) != null; ) {
-                stringList.add(line);
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                stringList.add(sc.nextLine());
             }
-        } catch (IOException e) {
-            System.out.println("Error: wrong path!\n" + e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.out.println("Nem talalhato file");
         }
+
         return stringList;
     }
 }
